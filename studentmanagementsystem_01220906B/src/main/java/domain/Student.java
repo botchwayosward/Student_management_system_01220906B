1a42d2f307469e668a6cf35d7321859786a757d5
botchwayosward <botchwayosward@gmail.com>
2026-02-18 01:25:02 +0000

    Generated Getters and Setters for Student model

--- studentmanagementsystem_01220906B/src/main/java/domain/Student.java
+++ studentmanagementsystem_01220906B/src/main/java/domain/Student.java
@@ -8,12 +8,84 @@ public class Student {
     private String programme;
     private int level;
     private double gpa;
-    private String email;          
+    private String email;
     private String phoneNumber;
     private String status;
     private LocalDate dateAdded;
 
     public Student() {
         this.dateAdded = LocalDate.now();
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
     }
 }
